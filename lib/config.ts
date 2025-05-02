48164fcf7cdcd35f7ddc950c0acad27e59ffcee9
brightwillies <bwboakye2@gmail.com>
2025-05-02 17:19:48 -0400

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@
 const config = {
     env: {
         apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
-        prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API_ENDPOINT!,
+        prodApiEndpoint: process.env.NEXT_PUBLIC_PROD_API!,
         imagekit: {
             publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_KEY!,
             urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
